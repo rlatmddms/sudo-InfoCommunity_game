75ca88a67407836054fca9d6843784e84001e58b
rlatmddms <sunny823r@gmail.com>
2024-10-13 15:40:55 +0900

    grade system

--- tmp/Assets/Scripts/GameTime.cs
+++ tmp/Assets/Scripts/GameTime.cs
@@ -6,24 +6,30 @@ using UnityEngine.UI;
 public class GameTime : MonoBehaviour
 {
     public Text textbar;
+    public Text grade_text;
+
     public string str;
+    public string grade_str;
     public int day = 2;
     public int[] calender; 
     public int year = 2025;
     public int month = 3;
+    public int grade = 0;
     void Start()
     {
         calender = new int[] { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
         day = 2;
         month = 3;
         year = 2025;
-        StartCoroutine(timer());
+        grade_text.gameObject.SetActive(true);
         textbar.gameObject.SetActive(true); // 텍스트 표시
+        StartCoroutine(timer());
     }
     private IEnumerator timer()
     {
         for (int d = 0; d < 1045; d++)
         {
+            if (month == 3 && day == 2) grade++;
             if (day > calender[month])
             {
                 day = 1;
@@ -34,7 +40,6 @@ public class GameTime : MonoBehaviour
                     year++;
                 }
             }
-            Debug.Log(month + "월 " + day + "일");
             str = "";
             str += year.ToString();
             str += "년 ";
@@ -44,6 +49,12 @@ public class GameTime : MonoBehaviour
             str += "일";
             textbar.text = str;
             day++;
+
+            grade_str = "";
+            grade_str += grade.ToString();
+            grade_str += "학년";
+            grade_text.text = grade_str;
+
             yield return new WaitForSeconds(2f);
         }
     }
